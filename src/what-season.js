c9ecf864d7125597ffed3c0df0184759ac5609e4
Anton Ryabov <antonryabovdev@gmail.com>
2024-12-17 11:43:31 +0300

    feat: finish what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -12,44 +12,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function getSeason(date) {
-  console.log(date);
   if (!arguments.length) {
     return 'Unable to determine the time of year!';
   }
-  if (
-    (date instanceof Array && date instanceof Object) ||
-    date.toString() === '[object Object]' ||
-    typeof date === 'string' ||
-    typeof date === 'number' ||
-    !date.getDate()
-  ) {
+  try {
+    if (!typeof date.toDateString() === 'string') {
+      throw new Error('Invalid date!');
+    }
+    let winter = [11, 0, 1];
+    let spring = [2, 3, 4];
+    let summer = [5, 6, 7];
+    let autumn = [8, 9, 10];
+    if (winter.includes(date.getMonth())) {
+      return 'winter';
+    }
+    if (spring.includes(date.getMonth())) {
+      return 'spring';
+    }
+    if (summer.includes(date.getMonth())) {
+      return 'summer';
+    }
+    if (autumn.includes(date.getMonth())) {
+      return 'autumn';
+    }
+  } catch {
     throw new Error('Invalid date!');
-  }
-  if (
-    date.getFullYear() === new Date().getFullYear() &&
-    date.getMonth() === new Date().getMonth() &&
-    date.getDate() === new Date().getDate()
-  ) {
-    throw new Error('Invalid date!');
-  }
-  if (date.getMilliseconds() === 0) {
-    throw new Error('Invalid date!');
-  }
-  let winter = [11, 0, 1];
-  let spring = [2, 3, 4];
-  let summer = [5, 6, 7];
-  let autumn = [8, 9, 10];
-  if (winter.includes(date.getMonth())) {
-    return 'winter';
-  }
-  if (spring.includes(date.getMonth())) {
-    return 'spring';
-  }
-  if (summer.includes(date.getMonth())) {
-    return 'summer';
-  }
-  if (autumn.includes(date.getMonth())) {
-    return 'autumn';
   }
 }
 
